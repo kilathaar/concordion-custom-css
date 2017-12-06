ccc99881f1c57d0ff0ab179fd335b3786e550f27
Jimmy Ljungberg <jimmy.ljungberg@umu.se>
2017-12-06 21:03:00 +0100

    Refactor to use inline classes

--- src/main/java/se/kilathaar/concordion/CustomCSS.java
+++ src/main/java/se/kilathaar/concordion/CustomCSS.java
@@ -3,24 +3,94 @@ package se.kilathaar.concordion;
 import org.concordion.api.Resource;
 import org.concordion.api.extension.ConcordionExtender;
 import org.concordion.api.extension.ConcordionExtension;
+import org.concordion.api.listener.*;
 
 import java.util.Properties;
 
 public class CustomCSS implements ConcordionExtension {
 	private final Properties properties;
+	private final String success = ".success";
+	private final String failure = ".failure";
 
 	public CustomCSS() {
 		properties = new ReadPropertyFile("/custom-css.properties").properties();
 	}
 
-	@SuppressWarnings("unused")
 	@Override
 	public void addTo(ConcordionExtender concordionExtender) {
-		concordionExtender.withAssertTrueListener(new AssertTrueStyle(properties));
-		concordionExtender.withAssertFalseListener(new AssertFalseStyle(properties));
-		concordionExtender.withAssertEqualsListener(new AssertEqualsStyle(properties));
-		concordionExtender.withRunListener(new RunStyle(properties));
+		styleAssertTrueEvents(concordionExtender);
+		styleAssertFalseEvents(concordionExtender);
+		styleAssertEqualsEvents(concordionExtender);
+		styleRunEvents(concordionExtender);
 		concordionExtender.withLinkedCSS(properties.getProperty("linked_css_file"), new Resource(properties.getProperty("linked_css_file")));
 		concordionExtender.withLinkedJavaScript(properties.getProperty("linked_javascript_file"), new Resource(properties.getProperty("linked_javascript_file")));
+	}
+
+	private void styleRunEvents(ConcordionExtender concordionExtender) {
+		concordionExtender.withRunListener(new RunListener() {
+			private static final String key = "run";
+
+			public void runStarted(RunStartedEvent event) {
+				event.getElement().addStyleClass(properties.getProperty(key + ".started"));
+			}
+
+			public void successReported(RunSuccessEvent event) {
+				event.getElement().addStyleClass(properties.getProperty(key + success));
+			}
+
+			public void failureReported(RunFailureEvent event) {
+				event.getElement().addStyleClass(properties.getProperty(key + failure));
+			}
+
+			public void ignoredReported(RunIgnoreEvent runIgnoreEvent) {
+				runIgnoreEvent.getElement().addStyleClass(properties.getProperty(key + ".ignore"));
+			}
+
+			public void throwableCaught(ThrowableCaughtEvent event) {
+				event.getElement().addStyleClass(properties.getProperty("throwable.caught"));
+			}
+		});
+	}
+
+	private void styleAssertEqualsEvents(ConcordionExtender concordionExtender) {
+		concordionExtender.withAssertEqualsListener(new AssertEqualsListener() {
+			private final String key = "assert_equals";
+
+			public void successReported(AssertSuccessEvent event) {
+				event.getElement().addStyleClass(properties.getProperty(key + success));
+			}
+
+			public void failureReported(AssertFailureEvent event) {
+				event.getElement().addStyleClass(properties.getProperty(key + failure));
+			}
+		});
+	}
+
+	private void styleAssertFalseEvents(ConcordionExtender concordionExtender) {
+		concordionExtender.withAssertFalseListener(new AssertFalseListener() {
+			private final String key = "assert_false";
+
+			public void successReported(AssertSuccessEvent event) {
+				event.getElement().addStyleClass(properties.getProperty(key + success));
+			}
+
+			public void failureReported(AssertFailureEvent event) {
+				event.getElement().addStyleClass(properties.getProperty(key + failure));
+			}
+		});
+	}
+
+	private void styleAssertTrueEvents(ConcordionExtender concordionExtender) {
+		concordionExtender.withAssertTrueListener(new AssertTrueListener() {
+			private final String key = "assert_true";
+
+			public void successReported(AssertSuccessEvent event) {
+				event.getElement().addStyleClass(properties.getProperty(key + success));
+			}
+
+			public void failureReported(AssertFailureEvent event) {
+				event.getElement().addStyleClass(properties.getProperty(key + failure));
+			}
+		});
 	}
 }
